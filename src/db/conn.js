244ec34190fd6ae829eb4153d7f84611246ef3c6
Satyam Kumar <satyam20112006@gmail.com>
2021-12-14 15:44:57 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-  .connect("mongodb://localhost:27017/theatre4u", {
+  .connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
